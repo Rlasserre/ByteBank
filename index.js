71abcc2acf56a8bcaa2c3607ded4fcbc07af9e06
Rafael Lasserre <80365790+Rlasserre@users.noreply.github.com>
2022-01-11 10:37:20 -0300

    add class-fields#private
    esse código é apenas para estudo, campos privados ainda não estão totalmente implementados na linguagem.

--- index.js
+++ index.js
@@ -1,29 +1,49 @@
 class Cliente {
-    nome;
-    cpf;
-    agencia;
-    saldo;
-    rg;
+	nome;
+	cpf;
+	conta;
+}
+
+class ContaCorrente {
+	agencia;
+	#saldo = 0;
+
+	sacar(valor) {
+		if (this.#saldo >= valor) {
+			this.#saldo -= valor;
+			return valor;
+		}
+	}
+
+	depositar(valor) {
+		if (valor > 0) {
+			this.#saldo += valor;
+
+		}
+	}
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-const cliente3 = new Cliente();
 
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
-cliente1.rg = 123456789;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-cliente3.nome = "Margarida";
-cliente3.cpf = 55522233309;
-cliente3.agencia = 1001;
-cliente3.saldo = 0;
 
-console.log(cliente1, cliente2, cliente3);
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.agencia = 1001;
+
+contaCorrenteRicardo.depositar(15);
+
+contaCorrenteRicardo.depositar(50);
+
+contaCorrenteRicardo.sacar(30);
+
+contaCorrenteRicardo.sacar(200);
+
+
+console.log(cliente1);
+//console.log(cliente2);
