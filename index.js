7f984402f1bb70abebaad6d661298a91583b362e
Rafael Lasserre <80365790+Rlasserre@users.noreply.github.com>
2022-01-18 17:01:19 -0300

    interfaces

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ import { Gerente } from "./src/Funcionarios/Gerente.js";
 import { Diretor } from "./src/Funcionarios/Diretor.js";
 import { SistemaAutenticacao } from "./src/SistemaAutenticacao.js";
 
-const gerente1 = new Gerente("Maraisa", 5000, 55522233311);
-const diretor1 = new Diretor("Rafael", 10000, 55522233311);
-diretor1.cadastrarSenha("12345678");
+const cliente = new Cliente("Rafa", 55532345871, 3584);
+const gerente = new Gerente("Maraisa", 5000, 55522233311);
+const diretor = new Diretor("Rafael", 10000, 55522233311);
+diretor.cadastrarSenha("12345678");
+gerente.cadastrarSenha("5678");
 
-const estaLogado = SistemaAutenticacao.login(diretor1, "12345678");
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "3584");
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "12345678");
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "5678");
 
-console.log(estaLogado);
+console.log(gerenteEstaLogado);
